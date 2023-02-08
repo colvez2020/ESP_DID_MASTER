c4d69fed080c2887ecf9c1d5dd761c61c53b9e00
deybar <deybar.mora@correounivalle.edu.co>
2023-02-08 10:39:05 -0500

    Update ESP_DID_MASTER.cpp

--- src/ESP_DID_MASTER.cpp
+++ src/ESP_DID_MASTER.cpp
@@ -5,6 +5,7 @@
 #include "CTRL_SONIC.h"
 #include "TRAMAS_COM.h"
 
+// Deybar estuvo aquí
 
 ULTRA_SONIC Soinc_DID(25);                    //Controla lectura de los ultrasonicos
 CONTROL_MSG Main_msg;                         //Crea 2 streams Serial (HDMI SW y BLUE HW) + MASTES I2C
